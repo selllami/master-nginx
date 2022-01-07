b323f24f76abe47f102b9e2630e0dd4243a0c651
LuoHao <hluo5206@gmail.com>
2022-01-07 13:15:45 +0800

    fix(server): expose 3000

--- dockerfile
+++ dockerfile
@@ -1,6 +1,5 @@
-FROM node:17-alpine
+FROM node:10.15
 COPY . /app
 WORKDIR /app
 RUN npm install --registry=https://registry.npm.taobao.org
 EXPOSE 3000
-
--- server/index.js
+++ server/index.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use('/static', express.static(path.join(__dirname, "assets")));
 
-app.listen(8090, 'localhost', function() {
-  console.log('server is running on 8080');
+app.listen(3000, 'localhost', function() {
+  console.log('server is running on 3000');
 });
